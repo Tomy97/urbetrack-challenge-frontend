4e516bb1436e96abb8329a27aef63753099f0e2f
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2026-06-28 03:30:15 -0300

    chore: configure prettier, typescript aliases and vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import babel from "@rolldown/plugin-babel";
 import path from "path";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
