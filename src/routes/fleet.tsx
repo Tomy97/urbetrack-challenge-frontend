2346f1eb143b939725303ec4b879145cf4ae88cb
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2026-06-28 17:02:59 -0300

    refactor(index): update page title to 'Urbetrack Ops' and format imports in fleet route

--- src/routes/fleet.tsx
+++ src/routes/fleet.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { FleetView } from '@/features/fleet/views'
+import { createFileRoute } from '@tanstack/react-router';
+import { FleetView } from '@/features/fleet/views';
 
 export const Route = createFileRoute('/fleet')({
   component: FleetView,
-})
+});
